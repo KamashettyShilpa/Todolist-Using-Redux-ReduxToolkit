ca5903def4ffad8daf9be7b5bafc7a164f8c6ec3
KamashettyShilpa <kshilpa0609@gmail.com>
2024-12-21 16:14:16 +0530

    Added styling

--- redux-class/src/components/AddForm.jsx
+++ redux-class/src/components/AddForm.jsx
@@ -1,22 +1,31 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addNewTodo } from "../features/todoLists/todoListsSlice";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 
-
-export default function AddForm(){
-    const [task,setTask]=useState("");
-    let dispatch=useDispatch();
-    let handleSub =(evt)=>{
+export default function AddForm() {
+    const [task, setTask] = useState("");
+    let dispatch = useDispatch();
+    let handleSub = (evt) => {
         evt.preventDefault();
         dispatch(addNewTodo(task));
         setTask("");
     }
-   
+
     return (
         <form onSubmit={handleSub}>
-            <input type="text"  onChange={(e)=>setTask(e.target.value)}></input>
-            <button>Add Task</button>
+
+            <TextField id="outlined-basic" label="Enter Task" variant="outlined" onChange={(e) => setTask(e.target.value)} InputLabelProps={{
+                style: { color: '#1972D2' },
+            }} inputProps={{
+                style: { color: '#1972D2' },
+            }} /> <div style={{ margin: '20px 0' }}></div>
+            <Button variant="contained" type="submit">Add task</Button>
+
         </form>
     );
 }
+
+
--- redux-class/src/components/TodoLists.jsx
+++ redux-class/src/components/TodoLists.jsx
@@ -1,33 +1,39 @@
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import AddForm from "./AddForm";
+import './TodoLists.css';
+import Button from '@mui/material/Button';
 
 import { deleteTodo, markAsDone } from "../features/todoLists/todoListsSlice";
 
-export function TodoLists(){
+export function TodoLists() {
 
-    const todoLists=useSelector((state)=>state.todoLists);
+    const todoLists = useSelector((state) => state.todoLists);
     console.log(todoLists);
-    const dispatch=useDispatch();
-    let handleDelete=(id)=>{
+    const dispatch = useDispatch();
+    let handleDelete = (id) => {
         dispatch(deleteTodo(id));
     }
-    let handleMarkAsDone=(id)=>{
-       dispatch(markAsDone(id));
+    let handleMarkAsDone = (id) => {
+        dispatch(markAsDone(id));
     }
     return (
-        <>
-        <AddForm/>
-        <h3>To do lists</h3>
-         <ul>
-            {
-                todoLists.map((todoList)=><li key={todoList.id} style={{textDecorationLine: todoList.isDone?"line-through":""}}>{todoList.task}&nbsp;&nbsp;<button onClick={()=>{
-                    handleDelete(todoList.id);
-                }}>Delete</button><button onClick={()=>{
-                    handleMarkAsDone(todoList.id)
-                }}>Mark as Done</button></li>
-                )
-            }
-         </ul>    
-    </>
+        <><h1>Todo List App</h1>
+            <AddForm />
+            <div className="ShowTasks">
+                <h2>Todo lists</h2>
+                <div className="lists">
+                    <ul>
+                        {
+                            todoLists.map((todoList) => <li key={todoList.id} ><span style={{ textDecorationLine: todoList.isDone ? "line-through" : "" }}>{todoList.task}</span>&nbsp;&nbsp;
+                                <Button variant="contained" onClick={() => { handleDelete(todoList.id); }} size='small'>Delete</Button>&nbsp;&nbsp;&nbsp;
+                                <Button variant="contained" onClick={() => { handleMarkAsDone(todoList.id) }} size='small'>Mark as Done</Button></li>
+
+                            )
+                        }
+
+                    </ul>
+                </div>
+            </div>
+        </>
     );
 }
